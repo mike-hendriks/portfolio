79b2c485c9db280a4df0b9d47468f3990a02b412
Mike Hendriks <mikehen6@gmail.com>
2020-12-09 15:19:53 +0100

    Favicon added

--- index.php
+++ index.php
@@ -10,11 +10,13 @@
 
 		gtag('config', 'UA-59300751-1');
 	</script>
+	<link rel="icon" type="image/png" href="inc/img/favicon.png"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="public/css/style.min.css">
 	<title>Mike Hendriks</title>
+	
 </head>
 <body>
 	<div class="burger_menu">
